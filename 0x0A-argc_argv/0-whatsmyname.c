fa91da14bf900a659b9b28f3f00a9603e66d3d20
10Nderitu <raynderitu10@gmail.com>
2023-03-11 01:26:05 +0300

    argc, argv C

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * Description: prints the name of the program
+ * Prints the name of the program
  * @argc: number of command line arguements
  * @argv: an array of command line arguements
  * Return: 0
